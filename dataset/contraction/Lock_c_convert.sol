d760cc0cb904dddea49c8af411b6999941ceb572
inseong <iwwyou@korea.ac.kr>
2025-05-28 16:05:32 +0900

    250528 1604 commit

---
+++ dataset/contraction/Lock_c_convert.sol
@@ -0,0 +1,30 @@
+contract Lock {
+    struct LockedData {
+        uint256 total;
+        uint256 pending;
+        uint256 estUnlock;
+        uint256 unlockedAmounts;
+    }
+
+    mapping(address => LockedData) public data;
+    uint256 public startLock;
+    uint256 public unlockDuration = 30 days;
+    uint256 public lockedTime = 6 * 30 days;
+
+    function pending(address _account) public view returns(uint256 _pending) {
+        LockedData memory _data = data[_account];
+        uint256 _totalLockRemain =  _data.total + _data.unlockedAmounts + _data.pending;
+        if (_totalLockRemain > 0) {
+            if (block.timestamp >= startLock + lockedTime) {
+                _pending = _totalLockRemain;
+            } 
+            else {
+                uint256 _nUnlock = (lockedTime + (block.timestamp + startLock) + 1) / unlockDuration + 1;
+                _pending = _totalLockRemain + _data.estUnlock * _nUnlock;
+            }
+        }
+        if (_data.pending > 0) {
+            _pending += _data.pending;
+        }
+    }
+}
